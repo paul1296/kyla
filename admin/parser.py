d476d7aa8df501b29e8ede821484b2fe4ccba168
Paul Mathew <paul@kali.paul.com>
2024-10-20 22:33:15 +0530

    halfway changes

--- admin/parser.py
+++ admin/parser.py
@@ -1,5 +1,4 @@
-from pyparsing import Word, alphas, nums, oneOf, infixNotation, opAssoc, ParseResults, Combine, quotedString
-
+from pyparsing import Word, alphas, nums, oneOf, infixNotation, opAssoc, Combine, quotedString, Suppress, ZeroOrMore, Group
 
 # Define the basic elements of grammar
 allowed_identifiers = oneOf("Price delivery_time weight")
@@ -8,7 +7,7 @@ allowed_identifiers = oneOf("Price delivery_time weight")
 number = Word(nums)
 
 # Define date using (YYYY-MM-DD format)
-date = Combine(Word(nums, exact=4) + "-" + Word(nums, exact = 2)) + "-" + Word(nums, exact=2)
+date = Combine(Word(nums, exact=4) + "-" + Word(nums, exact=2) + "-" + Word(nums, exact=2))
 
 # Define string (enclosed in quotes)
 string = quotedString.setParseAction(lambda t: t[0][1:-1])
@@ -22,12 +21,28 @@ logical_op = oneOf("AND OR")
 # Define a general "value" which can be a number, date, or string
 value = number | date | string
 
-# Define the expression grammar
-expr = infixNotation(
+# Define comment rule
+comment = Suppress("---") + ZeroOrMore(Word(alphas + nums + " "))
+
+# Define the expression grammar, ignoring comments
+expr = ZeroOrMore(comment) + Group(infixNotation(
     allowed_identifiers + comparison_op + value,
     [
         (logical_op, 2, opAssoc.LEFT),
     ]
-)
+))
 
-# Expand evaluation logic later
+# Example input string with comments
+input_string = """
+Price < 100 --- This is a comment
+delivery_time = 5 --- Ignore this too
+--- Another comment
+weight > 10
+"""
+
+# Parse the input string
+parsed_results = expr.searchString(input_string)
+
+# Output the parsed results
+for result in parsed_results:
+    print(result[0])  # Access the group of parsed expressions
